17c29d722076714d56f27d244c52a776b1440274
Lou Niddam <louniddam@gmail.com>
2019-12-03 16:37:09 +0100

    Initial Commit

---
+++ rodzina-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rodzina-app
+//
+//  Created by louniddam on 03/12/2019.
+//  Copyright © 2019 louniddam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
